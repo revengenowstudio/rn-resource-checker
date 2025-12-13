c1662809ad016afb0bb8b56726c93f284a914a4a
shuiping233 <1944680304@qq.com>
2025-12-13 20:18:07 +0800

    尝试修复sha256计算异常问题

--- src/utils/sha_tools.go
+++ src/utils/sha_tools.go
@@ -20,42 +20,34 @@ type fileResult struct {
 // 计算单个文件的 SHA-256，使用 io.SectionReader 进行分段读取
 func computeSHA256ForFile(filename string, resultChan chan<- fileResult, wg *sync.WaitGroup) {
 	defer wg.Done()
+
 	file, err := os.Open(filename)
 	if err != nil {
-		resultChan <- fileResult{
-			filename: filename,
-			hash:     nil,
-			err:      fmt.Errorf("failed to open file: %w", err),
-		}
+		resultChan <- fileResult{filename: filename, hash: nil, err: fmt.Errorf("open: %w", err)}
+		return // 关键
 	}
 	defer file.Close()
 
-	hasher := sha256.New()
-	buffer := make([]byte, 4096) // 使用4KB的缓冲区
+	hash := sha256.New()
+	buf := make([]byte, 4*1024)
 
 	for {
-		n, err := file.Read(buffer)
+		n, err := file.Read(buf)
+		if err != nil && err != io.EOF {
+			resultChan <- fileResult{filename: filename, hash: nil, err: fmt.Errorf("read: %w", err)}
+			return // 关键
+		}
+		if n > 0 {
+			hash.Write(buf[:n])
+		}
 		if err == io.EOF {
 			break
 		}
-		if err != nil {
-			resultChan <- fileResult{
-				filename: filename,
-				hash:     nil,
-				err:      fmt.Errorf("failed to read file: %w", err),
-			}
-
-		}
-
-		hasher.Write(buffer[:n])
-	}
-	hash := hasher.Sum(nil)
-	resultChan <- fileResult{
-		filename: filename,
-		hash:     hash,
-		err:      nil,
 	}
 
+	result := hash.Sum(nil)
+
+	resultChan <- fileResult{filename: filename, hash: result, err: nil}
 }
 
 // 并发计算多个文件的 SHA-256
